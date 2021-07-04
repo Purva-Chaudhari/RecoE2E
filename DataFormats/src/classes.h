f2ac2d6c25b7c9e80e5973dba6946255c810bf0c
Purva-Chaudhari <48947123+Purva-Chaudhari@users.noreply.github.com>
2021-07-04 11:31:54 -0700

    Delete DataFormats directory

--- DataFormats/src/classes.h
+++
@@ -1,5 +0,0 @@
-#include "DataFormats/Common/interface/Wrapper.h"
-#include "DataFormats/Common/interface/AssociationVector.h"
-#include "RecoE2E/DataFormats/interface/FrameCollections.h"
-
-#include <vector>
