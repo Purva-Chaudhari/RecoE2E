cdfd253058fc8cde622f57831602998d95ed4116
Purva-Chaudhari <48947123+Purva-Chaudhari@users.noreply.github.com>
2021-07-04 11:32:15 -0700

    Delete EGTagger directory

--- EGTagger/python/EGTagger_cfi.py
+++
@@ -1,7 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-EGTagger = cms.EDProducer('EGTagger'
-    , photonCollection = cms.InputTag('gedPhotons')
-    , EGFrames = cms.InputTag('EGFrames','EGFrames')
-    , EGModelName = cms.string('tfModels/e_vs_ph_model.pb')
-    )
